7dcf2943391816acefe6440b11c64a062fd48178
Justin Cook <jhcook@secnix.com>
2020-03-26 11:32:01 +0000

    add some error and usage handling

--- aws/awstags.sh
+++ aws/awstags.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
+#
+# A wrapper for awstags.py to inspect all regions and output to files
+#
+# Usage: $ ./awstags.sh <tag names> <profile>
+# Author: Justin Cook <jhcook@secnix.com>
+
+case $1 in
+  -h|--help)
+    printf "Usage: awstags.sh <tag names> <profile>\n"
+    exit 0
+    ;;
+esac
+
+if [ $# -lt 2 ]
+then
+  >&2 printf "Usage: awstags.sh <tag names> <profile>\n"
+  exit 1
+fi
 
 for region in `aws ec2 describe-regions --all-regions --query \
   "Regions[].{Name:RegionName}" --output text` ; do
