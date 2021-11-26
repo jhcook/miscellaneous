e88f41add589733b5309234c5bc65d6d154b82de
Justin Cook <jhcook@secnix.com>
2021-11-26 09:40:22 +0000

    a utility in Python3 to read last lines in a file using file i/o

---
+++ bin/last_lines.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+#
+# This function uses Python3 to begin reading at the end of file in discrete
+# blocks and carry on reading backwards incrementing until the specific number
+# of lines are found.
+#
+# Author: Justin Cook <jhcook@secnix.com>
+
+from os import SEEK_END, SEEK_SET
+from re import finditer
+from posixpath import expanduser
+
+# The amount of data to read in one go, i.e., each chunk
+block = 256
+
+def last_n_lines(file_name, num_lines=10):
+    ''' Return num_lines lines from file_name
+
+        Author: Justin Cook <jhcook@secnix.com>
+    '''
+    num_found = 0
+    lines = ''
+    offset = block
+    try:
+        with open(file_name, 'r') as f:    
+            while num_found < num_lines:
+                # https://stackoverflow.com/questions/21533391/seeking-from-end-of-file-throwing-unsupported-exception
+                f.seek(0, SEEK_END)
+                f.seek(f.tell() - offset, SEEK_SET) 
+                lines = f.read(block) + lines
+                num_found = len([s.start() for s in finditer('\n', lines)])
+                offset += block
+    except IOError as err:
+        raise err
+    return lines.split('\n')[-10:]
+        
+if __name__ == "__main__":
+    lines = last_n_lines(expanduser('~/aws_resource_types.txt'))
+    print("{}: {}".format(len(lines), lines))
