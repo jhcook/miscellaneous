d6948090f0dcb730487cf019456710aa5ecc5720
Justin Cook <jhcook@secnix.com>
2021-11-29 09:49:30 +0000

    handle content from stdin

--- bin/last_lines.py
+++ bin/last_lines.py
@@ -11,12 +11,26 @@
 # Author: Justin Cook <jhcook@secnix.com>
 
 from getopt import GetoptError, getopt
-from sys import argv, exit
+from sys import stdin, argv, exit
 from os import SEEK_END, SEEK_SET
 from re import finditer
+from io import StringIO
+import contextlib
 
 # The amount of data to read in one go, i.e., each chunk
 block = 256
+
+@contextlib.contextmanager
+def smart_open(filename=None):
+    if filename and filename != '-':
+        fh = open(filename, 'r')
+    else:
+        fh = StringIO(''.join(stdin.readlines()))
+    try:
+        yield fh
+    finally:
+        if fh is not stdin:
+            fh.close()
 
 def last_n_lines(file_name: str, num_lines: int=10):
     ''' Return num_lines lines from file_name
@@ -27,7 +41,7 @@ def last_n_lines(file_name: str, num_lines: int=10):
     lines = ''
     offset = block
     try:
-        with open(file_name, 'r') as f:    
+        with smart_open(file_name) as f:    
             while num_found < num_lines:
                 try:
                     f.seek(0, SEEK_END)
@@ -45,11 +59,15 @@ def last_n_lines(file_name: str, num_lines: int=10):
 if __name__ == "__main__":
     usage = "{} -n <number_of_lines> <file_name>".format(argv[0].rpartition('/')[2])
     number_lines = 10
+    ifile = None
     try:
         opts, args = getopt(argv[1:], "hn:")
+        ifile = args[0]
     except GetoptError:
         print(usage)
         exit (2)
+    except IndexError:
+        ifile = '-'
     for opt, arg in opts:
         if opt == '-h':
             print(usage)
@@ -61,7 +79,7 @@ if __name__ == "__main__":
                 print(err)
                 exit(4)
     try:
-        for line in last_n_lines(args[0], number_lines):
+        for line in last_n_lines(ifile, number_lines):
             print(line)
     except FileNotFoundError as err:
         print(err)
