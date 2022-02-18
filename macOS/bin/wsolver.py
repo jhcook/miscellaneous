4fd2deecf43e1771951c209f3d418d7e1ec83f1c
Justin Cook <jhcook@gmail.com>
2022-02-18 00:03:29 +0000

    add command line arguments and squash some bugs

--- macOS/bin/wsolver.py
+++ macOS/bin/wsolver.py
@@ -18,6 +18,8 @@
 #
 # Author: Justin Cook
 
+from sys import argv
+from argparse import ArgumentParser
 from re import compile
 from collections import Counter
 
@@ -26,23 +28,29 @@ word_length = 5
 class WordleSolver():
     
     dictionary = "wwords"
-    letters = ['1st', '2nd', '3rd', '4th', '5th']
+    letters = ['first', 'second', 'third', 'fourth', 'fifth']
     srch_str = potential_words = blacked_out = unknown_chars = None
 
     def __init__(self):
         self.potential_words = []
         self.blacked_out = set()
         self.unknown_chars = {i: set() for i in range(word_length)}
-        self.srch_str = ['[a-z]'] * word_length
+        self.srch_str = ['[a-z]{1}'] * word_length
 
-    def __user_prompt(self):
+    def __user_prompt(self, args):
         for i, l in enumerate(self.letters):
-            known = input(f"{l} known letter: ")
+            if not args:
+                known = input(f"{l} known letter: ")
+            else:
+                known = eval(f"args.{l}")
             if known.startswith('!'):
                 [self.unknown_chars[i].add(c) for c in known[1:]]
             elif known:
                 self.srch_str[i] = known
-        self.blacked_out.add(input("Known duds: "))
+        if not args:
+            [self.blacked_out.add(c) for c in input("Known duds: ")]
+        else:
+            [self.blacked_out.add(c) for c in args.dud]
     
     def __letter_frequency(self): 
         potential_words = {w: Counter(list(w)) for w in self.potential_words}
@@ -53,7 +61,7 @@ class WordleSolver():
 
     def __gen_search(self):
         for i, v in enumerate(self.srch_str):
-            if v != '[a-z]': continue
+            if not self.unknown_chars[i] or not self.blacked_out: continue
             chars = []
             for k in self.unknown_chars:
                 if i == k:
@@ -74,29 +82,54 @@ class WordleSolver():
             for line in d.readlines():
                 word = regex.search(line)
                 if word:
-                    commit = True
                     the_word = word.group()
+                    commit = True
                     if required_letters: # This should be replaced with lookaheads
                         for res in [c in the_word for c in required_letters]:
-                            if not res: commit = False
+                            if not res: 
+                                commit = False
+                                break
                     if self.blacked_out: # This should be replaced with lookaheads
                         for bo in [c in the_word for c in self.blacked_out]:
-                            if bo: commit = False
+                            if bo:
+                                commit = False
+                                break
                     if commit:
                         self.potential_words.append(the_word)
 
-    def play(self):
-        self.__user_prompt()
+    def play(self, args=None):
+        self.__user_prompt(args)
         self.__gen_search()
         self.__search_dictionary()
         self.__letter_frequency()
 
 if __name__ == "__main__":
+    if len(argv) > 1:
+        # Get command-line arguments
+        parser = ArgumentParser(prog='wsolver.py', usage='%(prog)s [options]',
+                                description="A tool to help solve Wordle.",
+                                epilog="...just like that!")
+        parser.add_argument('-a', '--first', type=str, default='',
+                            help='1st character hint')
+        parser.add_argument('-b', '--second', type=str, default='',
+                            help='2nd character hint')
+        parser.add_argument('-c', '--third', type=str, default='',
+                            help='3rd character hint')
+        parser.add_argument('-d', '--fourth', type=str, default='',
+                            help='4th character hint')
+        parser.add_argument('-e', '--fifth', type=str, default='',
+                            help='5th character hint')
+        parser.add_argument('-z', '--dud', type=str, default='',
+                            help='characters not in word')
+        args = parser.parse_args()
+    else:
+        args = None
+
     # Create solver
     wordle = WordleSolver()
     # Generate words
     try:
-        wordle.play()
+        wordle.play(args)
         print("Suggestions: {}".format(', '.join(wordle.potential_words)))
     except KeyboardInterrupt:
         pass
