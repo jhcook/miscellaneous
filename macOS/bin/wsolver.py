91bb94fd0b4eaec520713bd0861f5aebdcd04bcc
Justin Cook <jhcook@gmail.com>
2022-02-18 18:16:12 +0000

    use argparse for command line args

--- macOS/bin/wsolver.py
+++ macOS/bin/wsolver.py
@@ -7,13 +7,7 @@
 # Tested on macOS Monterey
 #
 # Example:
-# ./wsolver.py
-# 1st known letter: !l
-# 2nd known letter: !uc
-# 3rd known letter: u
-# 4th known letter: l
-# 5th known letter:
-# Known duds: nhoi
+# $ ./wsolver.py -w words -a \!l -b \!uc -cu -dl -z nhoi
 # Suggestions: cauld, caulk, cauls, crull
 #
 # Author: Justin Cook
@@ -27,19 +21,21 @@ word_length = 5
 
 class WordleSolver():
     
-    dictionary = "wwords"
     letters = ['first', 'second', 'third', 'fourth', 'fifth']
-    srch_str = potential_words = blacked_out = unknown_chars = None
+    dictionary = srch_str = potential_words = blacked_out = None
+    unknown_chars = interactive = None
 
-    def __init__(self):
+    def __init__(self, args):
         self.potential_words = []
         self.blacked_out = set()
         self.unknown_chars = {i: set() for i in range(word_length)}
         self.srch_str = ['[a-z]{1}'] * word_length
+        self.dictionary = args.words if args.words else "/usr/share/dict/words"
+        self.interactive = args.interactive
 
     def __user_prompt(self, args):
         for i, l in enumerate(self.letters):
-            if not args:
+            if self.interactive:
                 known = input(f"{l} known letter: ")
             else:
                 known = eval(f"args.{l}")
@@ -47,7 +43,7 @@ class WordleSolver():
                 [self.unknown_chars[i].add(c) for c in known[1:]]
             elif known:
                 self.srch_str[i] = known
-        if not args:
+        if self.interactive:
             [self.blacked_out.add(c) for c in input("Known duds: ")]
         else:
             [self.blacked_out.add(c) for c in args.dud]
@@ -62,8 +58,9 @@ class WordleSolver():
     def __gen_search(self):
         for i, v in enumerate(self.srch_str):
             if not self.unknown_chars[i] and not self.blacked_out: continue
-            self.srch_str[i] = "(?:(?![{}])[a-z]){{1}}".format(''.join(
-                        set.union(self.unknown_chars[i], self.blacked_out)))
+            if len(v) > 1:
+                self.srch_str[i] = "(?:(?![{}])[a-z]){{1}}".format(''.join(
+                            set.union(self.unknown_chars[i], self.blacked_out)))
 
     def __search_dictionary(self):
         """Consult known matched characters `self.srch_str` to narrow down
@@ -94,29 +91,30 @@ class WordleSolver():
         self.__letter_frequency()
 
 if __name__ == "__main__":
-    if len(argv) > 1:
-        # Get command-line arguments
-        parser = ArgumentParser(prog='wsolver.py', usage='%(prog)s [options]',
-                                description="A tool to help solve Wordle.",
-                                epilog="...just like that!")
-        parser.add_argument('-a', '--first', type=str, default='',
-                            help='1st character hint')
-        parser.add_argument('-b', '--second', type=str, default='',
-                            help='2nd character hint')
-        parser.add_argument('-c', '--third', type=str, default='',
-                            help='3rd character hint')
-        parser.add_argument('-d', '--fourth', type=str, default='',
-                            help='4th character hint')
-        parser.add_argument('-e', '--fifth', type=str, default='',
-                            help='5th character hint')
-        parser.add_argument('-z', '--dud', type=str, default='',
-                            help='characters not in word')
-        args = parser.parse_args()
-    else:
-        args = None
+    # Get command-line arguments
+    parser = ArgumentParser(prog='wsolver.py', usage='%(prog)s [options]',
+                            description="A tool to help solve Wordle.",
+                            epilog="...just like that!")
+    parser.add_argument('-a', '--first', type=str, default='',
+                        help='1st character hint')
+    parser.add_argument('-b', '--second', type=str, default='',
+                        help='2nd character hint')
+    parser.add_argument('-c', '--third', type=str, default='',
+                        help='3rd character hint')
+    parser.add_argument('-d', '--fourth', type=str, default='',
+                        help='4th character hint')
+    parser.add_argument('-e', '--fifth', type=str, default='',
+                        help='5th character hint')
+    parser.add_argument('-i', '--interactive', action='store_true',
+                        help='interactive session')
+    parser.add_argument('-w', '--words', type=str, default='',
+                        help='path to dictionary')
+    parser.add_argument('-z', '--dud', type=str, default='',
+                        help='characters not in word')
+    args = parser.parse_args()
 
     # Create solver
-    wordle = WordleSolver()
+    wordle = WordleSolver(args)
     # Generate words
     try:
         wordle.play(args)
