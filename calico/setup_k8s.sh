fd7045d58214c57b8961d05c01d78b03da5e53a3
Justin Cook <jhcook@secnix.com>
2022-03-18 16:45:49 +0000

    create more nodes and assign 6g memory each

--- calico/setup_k8s.sh
+++ calico/setup_k8s.sh
@@ -7,9 +7,19 @@
 #
 # Author: Justin Cook
 
-minikube --addons ingress,ingress-dns,metrics-server --network-plugin=cni \
-  --extra-config=kubeadm.pod-network-cidr=172.16.0.0/20 --memory=6g \
-  --kubernetes-version=v1.21.7 -p calico start
+minikube --addons ingress,ingress-dns,metrics-server \
+         --network-plugin=cni \
+         --extra-config=kubeadm.pod-network-cidr=172.16.0.0/20 \
+         --memory=6g \
+         --kubernetes-version=v1.21.7 \
+         --nodes=3 \
+         -p calico \
+         start
+
+if [ ! -d "/etc/resolver" ]
+then
+  sudo mkdir /etc/resolver
+fi
 
 sudo bash -c "cat - > /etc/resolver/minikube-test <<EOF
 domain test
