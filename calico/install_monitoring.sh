af8b66a6727c2a53f6ee6b66f3ef8d2cd664d69a
Justin Cook <jhcook@secnix.com>
2022-03-23 18:46:06 +0000

    fetch values from github

--- calico/install_monitoring.sh
+++ calico/install_monitoring.sh
@@ -15,9 +15,16 @@ helm repo update
 kubectl create namespace cattle-monitoring-system --dry-run=client -o yaml | \
   kubectl apply -f -
 
-# Install the required charts for rancher-monitoring
-helm upgrade --install=true --namespace=cattle-monitoring-system --timeout=10m0s --values=monitoring/values.yaml --version=100.1.0+up19.0.3 --wait=true rancher-monitoring-crd http://charts.rancher.io/assets/rancher-monitoring-crd/rancher-monitoring-crd-100.1.0+up19.0.3.tgz
-helm upgrade --install=true --namespace=cattle-monitoring-system --timeout=10m0s --values=monitoring/values.yaml --version=100.1.0+up19.0.3 --wait=true rancher-monitoring http://charts.rancher.io/assets/rancher-monitoring/rancher-monitoring-100.1.0+up19.0.3.tgz
+# Install the required charts for rancher-monitoring which is just upstream
+# Prometheus and Grafana operators et al with a bit of configuration
+helm upgrade --install=true --namespace=cattle-monitoring-system --timeout=10m0s \
+  --values=https://raw.githubusercontent.com/rancher/charts/release-v2.6.3-patch1/charts/rancher-monitoring/100.1.0%2Bup19.0.3/values.yaml \
+  --version=100.1.0+up19.0.3 --wait=true rancher-monitoring-crd \
+  http://charts.rancher.io/assets/rancher-monitoring-crd/rancher-monitoring-crd-100.1.0+up19.0.3.tgz
+helm upgrade --install=true --namespace=cattle-monitoring-system --timeout=10m0s \
+  --values=https://raw.githubusercontent.com/rancher/charts/release-v2.6.3-patch1/charts/rancher-monitoring/100.1.0%2Bup19.0.3/values.yaml \
+  --version=100.1.0+up19.0.3 --wait=true rancher-monitoring \
+  http://charts.rancher.io/assets/rancher-monitoring/rancher-monitoring-100.1.0+up19.0.3.tgz
 
 # Wait for all the deployments to become available
 for deploy in $(kubectl get deploy -n cattle-monitoring-system -o name)
