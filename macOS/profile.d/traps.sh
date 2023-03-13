e95a95d6662bae54e4cf21434acc6ec21ed61df1
Justin Cook <jhcook@secnix.com>
2023-03-13 13:14:07 +1100

    Add fix for Homebrew HOMEBREW_NO_INSTALL_FROM_API

---
+++ macOS/profile.d/traps.sh
@@ -0,0 +1,10 @@
+# Check to see if the watcher is running. If not, start it.
+if ! pgrep -qf "${HOME}/profile.d/bin/ipwatcher.py"
+then
+    "${HOME}/profile.d/bin/ipwatcher.py" &
+fi
+
+# The following trap for USR1 is used to source the environment after an IP
+# address change.
+# https://superuser.com/questions/1042748/periodically-source-shell-profile
+trap "source ~/.bash_profile" USR1
