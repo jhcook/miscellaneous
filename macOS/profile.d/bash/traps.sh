39f8bbf9337e97be6373face99f6afb83adc6f5e
Justin Cook <jhcook@secnix.com>
2023-06-17 11:05:51 +1000

    a bit simpler interpolation of variables in functions

--- macOS/profile.d/bash/traps.sh
+++ macOS/profile.d/bash/traps.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 # Check to see if the watcher is running. If not, start it.
 if ! pgrep -qf "${HOME}/profile.d/bin/ipwatcher.py"
 then
@@ -8,5 +7,4 @@ fi
 # The following trap for USR1 is used to source the environment after an IP
 # address change.
 # https://superuser.com/questions/1042748/periodically-source-shell-profile
-# shellcheck disable=SC1091
-trap 'source "${HOME}/.bash_profile"' USR1
+trap "source ~/.bash_profile" USR1
