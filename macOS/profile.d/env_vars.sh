2be61428b830b74cd8c788c072d892e0bc131057
Justin Cook <jhcook@secnix.com>
2023-06-28 16:37:30 +1000

    add VAGRANT_DEFAULT_PROVIDER

--- macOS/profile.d/env_vars.sh
+++ macOS/profile.d/env_vars.sh
@@ -1,10 +1,13 @@
-#export proxy_server="`ipconfig getifaddr en0`:3128"
-export proxy_server="127.0.0.1:3128"
+export proxy_server="`ipconfig getifaddr en1`:3128"
+#export proxy_server="127.0.0.1:3128"
 #export ALL_PROXY="http://${proxy_server}"
 export http_proxy="http://${proxy_server}"
 export HTTP_PROXY="http://${proxy_server}"
 export https_proxy="http://${proxy_server}"
 export HTTPS_PROXY="http://${proxy_server}"
-export no_proxy=".local,.test,.crc.testing,.testing,127.0.0.1,${WIFICIDR}"
-export NO_PROXY=".local,.test,.crc.testing,.testing,127.0.0.1,${WIFICIDR}"
+export no_proxy=".local,.crc.testing,.test,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,127.0.0.1,${WIFICIDR}"
+export NO_PROXY=".local,.crc.testing,.test,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,127.0.0.1,${WIFICIDR}"
+
+# Set default Vagrant provider
+export VAGRANT_DEFAULT_PROVIDER="vmware_desktop"
 
