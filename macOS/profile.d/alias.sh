b2b90a1d744f46080e11a9e9541f8806b87cead7
Justin Cook <jhcook@secnix.com>
2023-06-28 16:40:50 +1000

    move janitor to shell specific alias

--- macOS/profile.d/alias.sh
+++ macOS/profile.d/alias.sh
@@ -1,3 +1,2 @@
 alias brewski='brew update && brew upgrade ; brew cleanup; brew doctor'
 alias smashmp='for node in $(multipass list --format json | jq -r ".list[].name") ; do multipass delete $node --purge ; done'
-alias janitor='export PATH="/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin" ; . ~/.bash_profile'
---
+++ macOS/profile.d/zsh/alias.sh
@@ -0,0 +1,1 @@
+alias janitor='export PATH="/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin" ; unset KUBECONFIG ; . ~/.zshrc'
